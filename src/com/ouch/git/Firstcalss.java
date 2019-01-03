18b21612854211feea8789f30ab11375407a60f3
liukecheng <liukechengbao@qq.com>
2019-01-03 19:53:05 +0800

    zai zai zzai zai zai zai

---
+++ src/com/ouch/git/Firstcalss.java
@@ -0,0 +1,9 @@
+package com.ouch.git;
+
+public class Firstcalss {
+    public static void main(String[] args) {
+        System.out.println("远程仓库链接");
+        System.out.println("远程仓库链接");
+        System.out.println("远程仓库链接");
+    }
+}
